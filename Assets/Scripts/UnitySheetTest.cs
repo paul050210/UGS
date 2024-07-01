0bd8005891a23ae5f7d117e81aa4542917ab2128
paul050210 <leepaul050210@gmail.com>
2024-07-01 12:58:16 +0900

    대화스킵

--- Assets/Scripts/UnitySheetTest.cs
+++ Assets/Scripts/UnitySheetTest.cs
@@ -9,8 +9,9 @@ using System.Text;
 public class UnitySheetTest : MonoBehaviour
 {
     [SerializeField] private List<Text> textList;
-    private int index = 3;
-
+    private int index = 0;
+    private bool isTypingDone = false;
+    Dictionary<int, DefaultTable.Data> localeMap = new Dictionary<int, DefaultTable.Data>();
 
 
     private void Awake()
@@ -20,18 +21,37 @@ public class UnitySheetTest : MonoBehaviour
 
     private void Start()
     {
-        var localeMap = DefaultTable.Data.GetDictionary();
+        localeMap = DefaultTable.Data.GetDictionary();
+        SetText();   
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isTypingDone)
+            {
+                index++;
+                SetText();
+            }
+            else
+            {
+                isTypingDone = true;
+            }
+        }
+    }
+
+    private void SetText()
+    {
         try
         {
+            isTypingDone = false;
             StartCoroutine(TypeTextEffect(localeMap[index].strValue));
-            //textList[0].text = localeMap[index].strValue;
-            Debug.Log(localeMap[index].strValue);
         }
-        catch ( KeyNotFoundException )
+        catch (KeyNotFoundException)
         {
             Debug.LogError("인덱스값 오류");
         }
-        
     }
 
     IEnumerator TypeTextEffect(string text)
@@ -42,9 +62,16 @@ public class UnitySheetTest : MonoBehaviour
 
         for (int i = 0; i < text.Length; i++)
         {
+            if(isTypingDone)
+            {
+                textList[0].text = text;
+                break;       
+            }
             stringBuilder.Append(text[i]);
             textList[0].text = stringBuilder.ToString();
             yield return new WaitForSeconds(0.1f);
         }
+
+        isTypingDone = true;
     }
 }
