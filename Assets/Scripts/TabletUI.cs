853bbd5073fc69270456ed25ed48aa1fcd32f55c
paul050210 <leepaul050210@gmail.com>
2024-07-01 16:09:05 +0900

    테블릿UI

--- Assets/Scripts/TabletUI.cs
+++ Assets/Scripts/TabletUI.cs
@@ -1,22 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
+enum State
+{
+    Quest,
+    Inventory,
+    Setting
+};
 public class TabletUI : MonoBehaviour
 {
     [SerializeField] private Button tabeltButton;
-    [SerializeField] private Animator tabelAnimator;
+    [SerializeField] private Animator tabeltAnimator;
+    [SerializeField] private List<Button> stateButtons;
+    [SerializeField] private List<Canvas> tabeltCanvas;
+    private State currentState = State.Quest;
     
     private void Start()
     {
         tabeltButton.onClick.AddListener(SetAnimator);
+        for(int i = 0; i<stateButtons.Count; i++)
+        {
+            switch(i)
+            {
+                case 0:
+                    stateButtons[i].onClick.AddListener(() => 
+                    { 
+                        currentState = State.Quest;
+                        tabeltCanvas[0].gameObject.SetActive(true);
+                        tabeltCanvas[1].gameObject.SetActive(false);
+                        tabeltCanvas[2].gameObject.SetActive(false);    
+                    });
+                    break;
+                case 1:
+                    stateButtons[i].onClick.AddListener(() => 
+                    { 
+                        currentState = State.Inventory;
+                        tabeltCanvas[0].gameObject.SetActive(false);
+                        tabeltCanvas[1].gameObject.SetActive(true);
+                        tabeltCanvas[2].gameObject.SetActive(false);
+                    });
+                    break;
+                case 2:
+                    stateButtons[i].onClick.AddListener(() => 
+                    { 
+                        currentState = State.Setting;
+                        tabeltCanvas[0].gameObject.SetActive(false);
+                        tabeltCanvas[1].gameObject.SetActive(false);
+                        tabeltCanvas[2].gameObject.SetActive(true);
+                    });
+                    break;
+                default:
+                    break;
+            }
+            
+        }
     }
 
     private void SetAnimator()
     {
-        bool isOn = tabelAnimator.GetBool("IsOn");
-        tabelAnimator.SetBool("IsOn", !isOn);
+        bool isOn = tabeltAnimator.GetBool("IsOn");
+        tabeltAnimator.SetBool("IsOn", !isOn);
+        currentState = State.Quest;
+        tabeltCanvas[0].gameObject.SetActive(true);
+        for(int i = 0; i<stateButtons.Count; i++)
+        {
+            stateButtons[i].gameObject.SetActive(!isOn);
+        }
     }
     
 }
