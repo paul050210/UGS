d885bff0b79bc072d9c1a3a80bc699c600f5ea37
paul050210 <leepaul050210@gmail.com>
2024-07-04 11:14:36 +0900

    화면회전

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,14 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class CameraMove : MonoBehaviour
 {
+    private Vector3 dir = Vector3.zero;
+    private bool isMoving = false;
+    private float moveDelay = 0.7f;
+    [SerializeField] private GameObject[] canvases;
+
+
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.D))
         {
-            transform.Rotate(0, 90, 0);
+            if (isMoving) return;
+            isMoving = true;
+            canvases[Mathf.FloorToInt(dir.y / 90f) % 4].SetActive(false);
+            dir.y += 90f;
+            if (dir.y < 0) dir.y = 270f;
+            transform.DORotate(dir, moveDelay).SetEase(Ease.Linear).OnComplete(() => 
+            { 
+                isMoving = false;
+                canvases[Mathf.FloorToInt(dir.y / 90f) % 4].SetActive(true);
+            });
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (isMoving) return;
+            isMoving = true;
+            canvases[Mathf.FloorToInt(dir.y / 90f) % 4].SetActive(false);
+            dir.y -= 90f;
+            if (dir.y < 0) dir.y = 270f;
+            transform.DORotate(dir, moveDelay).SetEase(Ease.Linear).OnComplete(() => 
+            { 
+                isMoving = false;
+                canvases[Mathf.FloorToInt(dir.y / 90f) % 4].SetActive(true);
+            });
         }
     }
 }
