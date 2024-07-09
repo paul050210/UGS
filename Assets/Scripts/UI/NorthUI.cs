6c1c550cfbf242f830727b89a5c0434f452f0f03
paul050210 <leepaul050210@gmail.com>
2024-07-09 15:19:18 +0900

    ui 네비게이션 수정

--- Assets/Scripts/UI/NorthUI.cs
+++ Assets/Scripts/UI/NorthUI.cs
@@ -3,26 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
-using Unity.VisualScripting;
 
 public class NorthUI : MonoBehaviour
 {
     [SerializeField] private Button btn;
-    [SerializeField] private GameObject blackBox1;
-    [SerializeField] private GameObject blackBox2;
+    [SerializeField] private GameObject questWindow;
 
     private Camera cam;
-    private TabletUI tabletUI;
     private float fov = 60f;
     private float camY = 1f;
     private float camCloseDuration = 0.3f;
-
-
 
     private void Start()
     {
         cam = Camera.main;
         btn.onClick.AddListener(ZoomAndOut);
+        
     }
 
     private void ZoomAndOut()
@@ -36,15 +32,13 @@ public class NorthUI : MonoBehaviour
         {
             fov = 60f;
             camY = 1f;
-            blackBox1.SetActive(false);
-            blackBox2.SetActive(false);
+            questWindow.SetActive(false);
         }
         cam.DOFieldOfView(fov, camCloseDuration).SetEase(Ease.Linear);
         cam.gameObject.transform.DOMoveY(camY, camCloseDuration).SetEase(Ease.Linear).OnComplete(() => 
         {
-            if (fov != 40) return;
-            blackBox1.SetActive(true); 
-            blackBox2.SetActive(true);
+            if (fov == 40)
+                questWindow.SetActive(true); 
         });
     }
 }
