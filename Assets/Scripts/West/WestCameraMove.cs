e6183f59b631b8100fea281aee3fd73d1ebad2dc
jyooyyoo <jennyyoo0326@gmail.com>
2024-08-09 13:20:51 +0900

    west 카메라

--- Assets/Scripts/West/WestCameraMove.cs
+++ Assets/Scripts/West/WestCameraMove.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.dictionarys;
+using System.dictionarys.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
@@ -7,11 +7,11 @@ using DG.Tweening;
 public class WestCameraMove : MonoBehaviour
 {
     public GameObject mapPanel;
-    public GameObject collectionPanel;
+    public GameObject dictionaryPanel;
     public Button mapButton;
-    public Button collectionButton;
+    public Button dictionaryButton;
     public Button mapCloseButton;
-    public Button collectionCloseButton;
+    public Button dictionaryCloseButton;
     
 
     private Camera cam;
@@ -19,17 +19,17 @@ public class WestCameraMove : MonoBehaviour
     private float camCloseDuration = 0.3f;
     private Vector3 defaultCamPos = new Vector3(0, 1f, 0f);
     private Vector3 mapPanelCamPos = new Vector3(0f, 1.2f, 0f);
-    private Vector3 collectionPanelCamPos = new Vector3(0f, -1.4f, 0f);
+    private Vector3 dictionaryPanelCamPos = new Vector3(0f, -1.4f, 0f);
 
     void Start()
     {
         cam = Camera.main;
         mapPanel.SetActive(false);
-        collectionPanel.SetActive(false);
+        dictionaryPanel.SetActive(false);
         mapButton.onClick.AddListener(ShowMapPanel);
-        collectionButton.onClick.AddListener(ShowCollectionPanel);
+        dictionaryButton.onClick.AddListener(ShowDictionaryPanel);
         mapCloseButton.onClick.AddListener(CloseMapPanel);
-        collectionCloseButton.onClick.AddListener(CloseCollectionPanel);
+        dictionaryCloseButton.onClick.AddListener(CloseDictionaryPanel);
 
         // Ensure initial camera position
         cam.gameObject.transform.position = defaultCamPos;
@@ -43,14 +43,14 @@ public class WestCameraMove : MonoBehaviour
             cam.DOFieldOfView(fov, camCloseDuration).SetEase(Ease.Linear).OnComplete(() =>
             {
                 mapPanel.SetActive(true);
-                collectionPanel.SetActive(false);
-                collectionButton.interactable = false; // Disable the collection button
+                dictionaryPanel.SetActive(false);
+                dictionaryButton.interactable = false; // Disable the dictionary button
             });
             cam.gameObject.transform.DOMove(mapPanelCamPos, camCloseDuration).SetEase(Ease.Linear);
         }
     }
 
-    void ShowCollectionPanel()
+    void ShowDictionaryPanel()
     {
         if (Mathf.Approximately(cam.fieldOfView, 60f))
         {
@@ -58,24 +58,24 @@ public class WestCameraMove : MonoBehaviour
             cam.DOFieldOfView(fov, camCloseDuration).SetEase(Ease.Linear).OnComplete(() =>
             {
                 mapPanel.SetActive(false);
-                collectionPanel.SetActive(true);
+                dictionaryPanel.SetActive(true);
                 mapButton.interactable = false; // Disable the map button
                 
             });
-            cam.gameObject.transform.DOMove(collectionPanelCamPos, camCloseDuration).SetEase(Ease.Linear);
+            cam.gameObject.transform.DOMove(dictionaryPanelCamPos, camCloseDuration).SetEase(Ease.Linear);
         }
     }
 
     void CloseMapPanel()
     {
         mapPanel.SetActive(false);
-        collectionButton.interactable = true;
+        dictionaryButton.interactable = true;
         ResetCamera();
     }
 
-    void CloseCollectionPanel()
+    void CloseDictionaryPanel()
     {
-        collectionPanel.SetActive(false);
+        dictionaryPanel.SetActive(false);
         mapButton.interactable = true;
         ResetCamera();
     }
